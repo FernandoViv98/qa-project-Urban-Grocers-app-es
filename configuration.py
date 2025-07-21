c09c934837a6ce3efd7d6643c8c1f150166fb74a
Fernando Vivanco <fvc981@gmail.com>
2025-07-20 21:42:20 -0600

    URL ACTUALIZADO
    URL ACTUALIZADO

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-f9fd66fe-848a-4e7a-96cd-9f919895cb69.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-710f5a91-1ed0-4888-971c-0424761d5c13.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
